3cb26f56b483877becc86fc0cd597c0c4222eebd
Solarev Ruslan <ruslan251207@gmail.com>
2023-03-26 10:16:39 +1000

    added models, auto templates, hyperlinks and fixtures

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'products'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('products/', views.products, name='products'),
+    path('products/', views.products, name='catalog'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from .models import Product, ProductCategory
 # Create your views here.
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'store'
+    }
+    return render(request, 'products/index.html', context)
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'store - каталог',
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
+    }
+    return render(request, 'products/products.html', context)
